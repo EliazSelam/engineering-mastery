b06b8e62caf3e68aca11b6eb35a0f8616fefa36a
Eliaz Selam <eliaz.es1234@gmail.com>
2026-05-26 17:16:12 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import { Router } from 'wouter';
 import { useHashLocation } from 'wouter/use-hash-location';
+import { inject } from '@vercel/analytics';
 import App from './App.tsx';
 import './index.css';
+
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
